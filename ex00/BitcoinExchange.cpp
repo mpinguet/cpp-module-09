f225565928643bb93448daea31ed447462d78275
Maceo Pinguet <mpinguet@c4r2p5.42nice.fr>
2026-02-19 16:42:05 +0100

    allez

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -34,9 +34,80 @@ void BitcoinExchange::loadMap()
 	}
 }
 
+int toDouble(std::string &str)
+{
+	std::stringstream ss(str);
+	double i;
+	ss >> i;
+
+	return (i);
+}
+
 int parse_date(std::string date)
 {
-	
+	size_t i = 0;
+	std::string year;
+	std::string month;
+	std::string day;
+
+	while(isdigit(date[i]) || date[i] == '-')
+		i++;
+	if (i != date.size())
+		return (1);
+	year = date.substr(0, date.find('-'));
+	month = date.substr(date.find('-') + 1, date.find('-') - 2);
+	day = date.substr(date.find('-') + 4);
+
+	if (toDouble(year) < 2009 || toDouble(year) > 2022)
+		return (1);
+	if (toDouble(month) < 1 || toDouble(month) > 12)
+		return (1);
+	if (toDouble(day) < 1 || toDouble(day) > 31)
+		return (1);
+
+	return (0);
+}
+
+int parse_value(std::string value, std::string line)
+{
+	size_t i = 0;
+	int sign = 0;
+	int dot = 0;
+
+	while (isspace(value[i]))
+		i++;
+	while(i != value.size())
+	{
+		if (value[i] == '-' || value[i] == '+' || isdigit(value[i]) || value[i] == '.')
+		{
+			if (value[i] == '.')
+				dot += 1;
+			if (value[i] == '-' || value[i] == '+')
+				sign += 1;
+			if (sign == 2 || dot == 2)
+			{
+				std::cout << "Error: bad input => " << line << std::endl;
+				return (1);
+			}
+		}
+		else
+		{
+			std::cout << "Error: bad input => " << line << std::endl;
+			return (1);
+		}
+		i++;
+	}
+	if (toDouble(value) > 1000)
+	{
+		std::cout << "Error: too large a number." << std::endl;
+		return (1);
+	}
+	if (toDouble(value) < 0)
+	{
+		std::cout << "Error: not a positive number." << std::endl;
+		return (1);
+	}
+	return (0);
 }
 
 void BitcoinExchange::processInput(std::string filename)
@@ -62,6 +133,14 @@ void BitcoinExchange::processInput(std::string filename)
 		{
 			part1 = line.substr(0, line.find('|') - 1);
 			part2 = line.substr(line.find('|') + 2);
+			if (parse_date(part1))
+			{
+				std::cout << "Error: bad input => " << line << std::endl;
+				continue;
+			}
+			if (parse_value(part2, line))
+				continue;
+			
 		}
 	}
 	return ;
